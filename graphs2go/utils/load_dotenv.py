35e55567023c8b1497a7111bcf031f556d7401ba
Minor Gordon <minorg@server.fake>
2024-03-13 11:21:21 -0400

    load_dotenv: try to load .gitignored .env.local before .env.docker

--- graphs2go/utils/load_dotenv.py
+++ graphs2go/utils/load_dotenv.py
@@ -4,9 +4,12 @@ import dotenv
 
 
 def load_dotenv() -> None:
+    repository_root_dir_path = Path(__file__).parent.parent.parent.parent
+
     for dotenv_file_path in (
-        Path(__file__).parent.parent / ".env",
-        Path(__file__).parent.parent.parent.parent / ".env.docker",
+        repository_root_dir_path / ".env.local",
+        repository_root_dir_path / ".env.docker",
     ):
         if dotenv_file_path.is_file():
             dotenv.load_dotenv(dotenv_file_path)
+            return
