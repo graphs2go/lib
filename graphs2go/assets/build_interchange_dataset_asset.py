93bf3ffdd4d842a48d6690bfeb8612844801c4a2
Minor Gordon <minorg@server.fake>
2024-03-17 12:46:11 -0400

    shorten member variable names

--- graphs2go/assets/build_interchange_dataset_asset.py
+++ graphs2go/assets/build_interchange_dataset_asset.py
@@ -1,15 +1,24 @@
 from collections.abc import Iterable
 
-from dagster import AssetsDefinition, asset
+from dagster import AssetsDefinition, asset, get_dagster_logger
+from pathvalidate import sanitize_filename
 from graphs2go.models.interchange.dataset import Dataset
 from graphs2go.models.interchange.model import Model
 from graphs2go.resources.interchange_config import InterchangeConfig
 
 
-def build_interchange_dataset_asset(*, models: Iterable[Model]) -> AssetsDefinition:
+def build_interchange_dataset_asset(
+    *, interchange_dataset_name: str, interchange_models: Iterable[Model]
+) -> AssetsDefinition:
     @asset(code_version="1")
     def interchange_dataset(interchange_config: InterchangeConfig) -> Dataset:
+        logger = get_dagster_logger()
 
-        pass
+        store_path = (
+            interchange_config.parse().interchange_directory_path
+            / sanitize_filename(interchange_dataset_name)
+        )
+        if store_path.is_dir():
+            logger.info("interchange store %s already exists", interchange)
 
     return _asset
--- graphs2go/resources/interchange_config.py
+++ graphs2go/resources/interchange_config.py
@@ -13,19 +13,20 @@ class InterchangeConfig(ConfigurableResource):  # type: ignore
     @dataclass(frozen=True)
     class Parsed:
         interchange_directory_path: Path
+        recreate: bool
 
     interchange_directory_path: str
+    recreate: bool
 
     @classmethod
     def default(cls) -> InterchangeConfig:
-        return InterchangeConfig(
-            interchange_directory_path="",
-        )
+        return InterchangeConfig(interchange_directory_path="", recreate=False)
 
     @classmethod
     def from_env_vars(cls) -> InterchangeConfig:
         return cls(
             interchange_directory_path=EnvVar("INTERCHANGE_DIRECTORY_PATH").get_value(""),  # type: ignore
+            recreate=EnvVar.int("INTERCHANGE_RECREATE").get_value() == 1,
         )
 
     def parse(self) -> Parsed:
@@ -33,5 +34,6 @@ class InterchangeConfig(ConfigurableResource):  # type: ignore
             interchange_directory_path=parse_directory_path_config_value(
                 self.interchange_directory_path,
                 default=Path(__file__).parent.parent.parent / "data" / "interchange",
-            )
+            ),
+            recreate=self.recreate,
         )
