88fa6542a7fa65b767638ea40f8743ded7a12425
Minor Gordon <minorg@server.fake>
2024-03-18 15:56:48 -0400

    factor out RdfResourceBackedModel

--- graphs2go/models/interchange/model.py
+++ graphs2go/models/interchange/model.py
@@ -1,23 +1,5 @@
-from rdflib import RDF, Graph, URIRef
-from rdflib.resource import Resource
+from graphs2go.models.rdf_resource_backed_model import RdfResourceBackedModel
 
 
-class Model:
-    def __init__(self, *, resource: Resource):
-        if not isinstance(resource.identifier, URIRef):
-            raise ValueError("model resource must be named")  # noqa: TRY004
-        self.__resource = resource
-
-    @staticmethod
-    def _create_resource(*, type_: URIRef, uri: URIRef) -> Resource:
-        resource = Graph().resource(uri)
-        resource.add(RDF.type, type_)
-        return resource
-
-    @property
-    def resource(self) -> Resource:
-        return self.__resource
-
-    @property
-    def uri(self) -> URIRef:
-        return self.__resource.identifier
+class Model(RdfResourceBackedModel):
+    pass
---
+++ graphs2go/models/rdf_resource_backed_model.py
@@ -0,0 +1,23 @@
+from rdflib import RDF, Graph, URIRef
+from rdflib.resource import Resource
+
+
+class RdfResourceBackedModel:
+    def __init__(self, *, resource: Resource):
+        if not isinstance(resource.identifier, URIRef):
+            raise ValueError("model resource must be named")  # noqa: TRY004
+        self.__resource = resource
+
+    @staticmethod
+    def _create_resource(*, type_: URIRef, uri: URIRef) -> Resource:
+        resource = Graph().resource(uri)
+        resource.add(RDF.type, type_)
+        return resource
+
+    @property
+    def resource(self) -> Resource:
+        return self.__resource
+
+    @property
+    def uri(self) -> URIRef:
+        return self.__resource.identifier
