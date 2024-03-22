3e747c67a61db3356d6bd61b340732990ce863fa
Minor Gordon <minorg@server.fake>
2024-03-22 19:19:38 -0400

    add created/modified to model builder

--- graphs2go/models/interchange/model.py
+++ graphs2go/models/interchange/model.py
@@ -1,5 +1,15 @@
+from datetime import date, datetime
 from graphs2go.models import rdf
+from typing import Self
+from rdflib import DCTERMS, Literal
 
 
 class Model(rdf.Model):
-    pass
+    class Builder(rdf.Model.Builder):
+        def set_created(self, created: date | datetime) -> Self:
+            self._resource.set(DCTERMS.created, Literal(created))
+            return self
+
+        def set_modified(self, modified: date | datetime) -> Self:
+            self._resource.set(DCTERMS.modified, Literal(modified))
+            return self
