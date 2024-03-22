a7eaee146d23221ddf11c53b0f8f5cdcf7c3fd69
Minor Gordon <minorg@server.fake>
2024-03-22 19:19:51 -0400

    first pass at supporting ConceptScheme

--- graphs2go/transformers/transform_interchange_graph_to_skos_models.py
+++ graphs2go/transformers/transform_interchange_graph_to_skos_models.py
@@ -11,39 +11,44 @@ def transform_interchange_graph_to_skos_models(
 ) -> Iterable[skos.Model]:
     logger = get_dagster_logger()
 
-    for interchange_node in interchange_graph.nodes(rdf_type=SKOS.Concept):
-        skos_concept_builder = skos.Concept.builder(uri=interchange_node.uri)
+    for interchange_node_rdf_type in (SKOS.Concept, SKOS.ConceptScheme):
+        for interchange_node in interchange_graph.nodes(
+            rdf_type=interchange_node_rdf_type
+        ):
+            skos_concept_builder = skos.Concept.builder(uri=interchange_node.uri)
 
-        for interchange_label in interchange_node.labels:
-            if interchange_label.type == interchange.Label.Type.ALTERNATIVE:
-                skos_concept_builder.add_alt_label(interchange_label.literal_form)
-                skos_label = skos.Label.builder(
-                    literal_form=interchange_label.literal_form,
-                    uri=interchange_label.uri,
-                ).build()
-                yield skos_label
-                skos_concept_builder.add_alt_label(skos_label)
-            elif interchange_label.type == interchange.Label.Type.PREFERRED:
-                skos_concept_builder.add_pref_label(interchange_label.literal_form)
-                skos_label = skos.Label.builder(
-                    literal_form=interchange_label.literal_form,
-                    uri=interchange_label.uri,
-                ).build()
-                yield skos_label
-                skos_concept_builder.add_pref_label(skos_label)
-            else:
-                logger.warning(
-                    "interchange concept %s has non-SKOS label %s",
-                    interchange_node.uri,
-                    interchange_label.uri,
-                )
-                continue
+            for interchange_label in interchange_node.labels:
+                if interchange_label.type == interchange.Label.Type.ALTERNATIVE:
+                    skos_concept_builder.add_alt_label(interchange_label.literal_form)
+                    skos_label = skos.Label.builder(
+                        literal_form=interchange_label.literal_form,
+                        uri=interchange_label.uri,
+                    ).build()
+                    yield skos_label
+                    skos_concept_builder.add_alt_label(skos_label)
+                elif interchange_label.type == interchange.Label.Type.PREFERRED:
+                    skos_concept_builder.add_pref_label(interchange_label.literal_form)
+                    skos_label = skos.Label.builder(
+                        literal_form=interchange_label.literal_form,
+                        uri=interchange_label.uri,
+                    ).build()
+                    yield skos_label
+                    skos_concept_builder.add_pref_label(skos_label)
+                else:
+                    logger.warning(
+                        "interchange concept %s has non-SKOS label %s",
+                        interchange_node.uri,
+                        interchange_label.uri,
+                    )
+                    continue
 
-        for interchange_relationship in interchange_node.relationships:
-            if interchange_relationship.predicate.startswith(SKOS._NS):  # noqa: SLF001
-                skos_concept_builder.add_relationship(
-                    object_=interchange_relationship.object,
-                    predicate=interchange_relationship.predicate,
-                )
+            for interchange_relationship in interchange_node.relationships:
+                if interchange_relationship.predicate.startswith(
+                    SKOS._NS
+                ):  # noqa: SLF001
+                    skos_concept_builder.add_relationship(
+                        object_=interchange_relationship.object,
+                        predicate=interchange_relationship.predicate,
+                    )
 
-        yield skos_concept_builder.build()
+            yield skos_concept_builder.build()
