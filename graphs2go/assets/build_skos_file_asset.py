b31e8b7de3168ddc3283e9b2648078b1b9302cb6
Minor Gordon <minorg@server.fake>
2024-03-23 12:48:32 -0400

    output multiple RDF formats

--- graphs2go/assets/build_skos_file_asset.py
+++ graphs2go/assets/build_skos_file_asset.py
@@ -1,23 +1,24 @@
 from dagster import AssetsDefinition, PartitionsDefinition, asset
 
 from graphs2go.loaders.rdf_directory_loader import RdfDirectoryLoader
-from graphs2go.models import skos
+from graphs2go.models import rdf, skos
 from graphs2go.resources.output_config import OutputConfig
 
 
 def build_skos_file_asset(
-    *, partitions_def: PartitionsDefinition | None = None
+    *,
+    partitions_def: PartitionsDefinition | None = None,
+    rdf_formats: tuple[rdf.Format, ...] = (rdf.Format.NQUADS,)
 ) -> AssetsDefinition:
     @asset(code_version="1", partitions_def=partitions_def)
     def skos_file(
         output_config: OutputConfig, skos_graph: skos.Graph.Descriptor
     ) -> None:
-        output_config_parsed = output_config.parse()
-
-        with RdfDirectoryLoader.create(
-            directory_path=output_config_parsed.directory_path / "skos",
-            rdf_format=output_config_parsed.rdf_format,
-        ) as loader, skos.Graph.open(skos_graph) as open_skos_graph:
-            loader.load(open_skos_graph.to_rdflib_graph())
+        for rdf_format in rdf_formats:
+            with RdfDirectoryLoader.create(
+                directory_path=output_config.parse().directory_path / "skos",
+                rdf_format=rdf_format,
+            ) as loader, skos.Graph.open(skos_graph) as open_skos_graph:
+                loader.load(open_skos_graph.to_rdflib_graph())
 
     return skos_file
