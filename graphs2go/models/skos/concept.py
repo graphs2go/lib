7d6039ae020b6ef349f627cfc66e952258d444e6
Minor Gordon <minorg@server.fake>
2024-03-23 15:06:10 -0400

    transform concept scheme

--- graphs2go/models/skos/concept.py
+++ graphs2go/models/skos/concept.py
@@ -2,26 +2,17 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Self
 
-from rdflib import SKOS, Literal, URIRef
+from rdflib import SKOS, URIRef
 
 from graphs2go.models.skos.concept_scheme import ConceptScheme
-from graphs2go.models.skos.label import Label
-from graphs2go.models.skos.model import Model
-from graphs2go.namespaces.skosxl import SKOSXL
+from graphs2go.models.skos.labeled_model import LabeledModel
 
 if TYPE_CHECKING:
     from collections.abc import Iterable
 
 
-class Concept(Model):
-    class Builder(Model.Builder):
-        def add_alt_label(self, alt_label: Label | Literal | URIRef) -> Self:
-            return self.__add_label(
-                label=alt_label,
-                skos_predicate=SKOS.altLabel,
-                skosxl_predicate=SKOSXL.altLabel,
-            )
-
+class Concept(LabeledModel):
+    class Builder(LabeledModel.Builder):
         def add_broader(self, broader: Concept | URIRef) -> Self:
             return self.add_relationship(object_=broader, predicate=SKOS.broader)
 
@@ -33,30 +24,6 @@ class Concept(Model):
             else:
                 raise TypeError(type(in_scheme))
             return self
-
-        def __add_label(
-            self,
-            *,
-            label: Label | Literal | URIRef,
-            skos_predicate: URIRef,
-            skosxl_predicate: URIRef,
-        ) -> Self:
-            if isinstance(label, Label):
-                self._resource.add(skosxl_predicate, label.uri)
-            elif isinstance(label, Literal):
-                self._resource.add(skos_predicate, label)
-            elif isinstance(label, URIRef):
-                self._resource.add(skosxl_predicate, label)
-            else:
-                raise TypeError(type(label))
-            return self
-
-        def add_pref_label(self, pref_label: Label | Literal | URIRef) -> Self:
-            return self.__add_label(
-                label=pref_label,
-                skos_predicate=SKOS.prefLabel,
-                skosxl_predicate=SKOSXL.prefLabel,
-            )
 
         def add_relationship(
             self, *, object_: Concept | URIRef, predicate: URIRef
@@ -73,15 +40,6 @@ class Concept(Model):
             return Concept(resource=self._resource)
 
     _CONCEPT_SCHEME_CLASS = ConceptScheme
-    _LABEL_CLASS = Label
-
-    @property
-    def alt_label(self) -> Iterable[Label | Literal]:
-        yield from self._values(SKOS.altLabel, self._map_term_to_literal)
-        yield from self._values(
-            SKOSXL.altLabel,
-            lambda term: self._map_term_to_model(self._LABEL_CLASS, term),
-        )  # type: ignore
 
     @property
     def broader(self) -> Iterable[Concept | URIRef]:
@@ -115,14 +73,6 @@ class Concept(Model):
             lambda term: self._map_term_to_model_or_uri(
                 self._CONCEPT_SCHEME_CLASS, term
             ),
-        )  # type: ignore
-
-    @property
-    def pref_label(self) -> Iterable[Label | Literal]:
-        yield from self._values(SKOS.prefLabel, self._map_term_to_literal)
-        yield from self._values(
-            SKOSXL.prefLabel,
-            lambda term: self._map_term_to_model(self._LABEL_CLASS, term),
         )  # type: ignore
 
     @classmethod
---
+++ graphs2go/models/skos/labeled_model.py
@@ -0,0 +1,58 @@
+from collections.abc import Iterable
+from typing import Self
+from rdflib import SKOS, Literal, URIRef
+from graphs2go.models.skos.label import Label
+from graphs2go.models.skos.model import Model
+from graphs2go.namespaces.skosxl import SKOSXL
+
+
+class LabeledModel(Model):
+    class Builder(Model.Builder):
+        def add_alt_label(self, alt_label: Label | Literal | URIRef) -> Self:
+            return self.__add_label(
+                label=alt_label,
+                skos_predicate=SKOS.altLabel,
+                skosxl_predicate=SKOSXL.altLabel,
+            )
+
+        def __add_label(
+            self,
+            *,
+            label: Label | Literal | URIRef,
+            skos_predicate: URIRef,
+            skosxl_predicate: URIRef,
+        ) -> Self:
+            if isinstance(label, Label):
+                self._resource.add(skosxl_predicate, label.uri)
+            elif isinstance(label, Literal):
+                self._resource.add(skos_predicate, label)
+            elif isinstance(label, URIRef):
+                self._resource.add(skosxl_predicate, label)
+            else:
+                raise TypeError(type(label))
+            return self
+
+        def add_pref_label(self, pref_label: Label | Literal | URIRef) -> Self:
+            return self.__add_label(
+                label=pref_label,
+                skos_predicate=SKOS.prefLabel,
+                skosxl_predicate=SKOSXL.prefLabel,
+            )
+
+    _LABEL_CLASS = Label
+
+    @property
+    def alt_label(self) -> Iterable[Label | Literal]:
+        yield from self._values(SKOS.altLabel, self._map_term_to_literal)
+        yield from self._values(
+            SKOSXL.altLabel,
+            lambda term: self._map_term_to_model(self._LABEL_CLASS, term),
+        )  # type: ignore
+
+    @property
+    def pref_label(self) -> Iterable[Label | Literal]:
+        yield from self._values(SKOS.prefLabel, self._map_term_to_literal)
+        yield from self._values(
+            SKOSXL.prefLabel,
+            lambda term: self._map_term_to_model(self._LABEL_CLASS, term),
+        )  # type: ignore
