6fe278d3fbc54ee70f2fe71736f4b9928d35ecd5
Minor Gordon <minorg@server.fake>
2024-03-26 11:41:51 -0400

    refactor into parallel transform function

---
+++ graphs2go/transformers/parallel_transform.py
@@ -0,0 +1,110 @@
+from collections.abc import Callable, Iterable
+from multiprocessing import JoinableQueue, Queue, Process
+from typing import TypeVar
+
+from dagster import get_dagster_logger
+
+_InputT = TypeVar("_InputT")
+_OutputT = TypeVar("_OutputT")
+_OutputQueue = Queue
+_WorkQueue = JoinableQueue
+
+_Consumer = [[_InputT, _OutputQueue, _WorkQueue], None]
+_Producer = Callable[[_InputT, _WorkQueue], None]
+
+
+def _consumer_wrapper(
+    consumer: _Consumer,
+    input_: _InputT,
+    output_queue: _OutputQueue,
+    work_queue: _WorkQueue,
+) -> None:
+    consumer(input_, output_queue, work_queue)
+
+    output_queue.put(None)  # Signal this consumer is done
+
+
+def _producer_wrapper(
+    consumer_count: int,
+    input_: _InputT,
+    producer: _Producer,
+    work_queue: _WorkQueue,
+) -> None:
+    producer(input_, work_queue)
+
+    for _ in range(consumer_count):
+        work_queue.put(None)  # Signal to the consumers there's no more work
+
+    work_queue.join()
+
+
+def parallel_transform(
+    *,
+    consumer: _Consumer,
+    input_: _InputT,
+    producer: _Producer,
+) -> Iterable[_OutputT]:
+    """
+    Generic function for performing parallel transformation of an input to an output.
+    """
+
+    logger = get_dagster_logger()
+    output_queue = Queue()
+    work_queue = JoinableQueue()
+
+    consumer_count = 2  # os.cpu_count()
+    logger.info(
+        "starting %d transformation consumer processes",
+        consumer_count,
+    )
+    consumer_processes = tuple(
+        Process(
+            target=_consumer_wrapper,
+            args=(
+                consumer,
+                input_,
+                output_queue,
+                work_queue,
+            ),
+        )
+        for _ in range(consumer_count)
+    )
+    for consumer_process in consumer_processes:
+        consumer_process.start()
+    logger.info(
+        "started %d transformation consumer processes",
+        consumer_count,
+    )
+
+    logger.info("starting transformation producer process")
+    producer_process = Process(
+        target=_producer_wrapper,
+        args=(
+            consumer_count,
+            input_,
+            producer,
+            work_queue,
+        ),
+    )
+    producer_process.start()
+    logger.info("started transformation producer process")
+
+    exited_consumer_count = 0
+    while True:
+        output_model_batch: tuple[_OutputT] | None = output_queue.get()
+
+        if output_model_batch is None:
+            exited_consumer_count += 1
+            logger.info(
+                "%d interchange graph transformation consumers exited",
+                exited_consumer_count,
+            )
+            if exited_consumer_count == consumer_count:
+                break
+            continue
+
+        yield from output_model_batch
+
+    producer_process.join()
+    for consumer_process in consumer_processes:
+        consumer_process.join()
--- graphs2go/transformers/transform_interchange_graph.py
+++ graphs2go/transformers/transform_interchange_graph.py
@@ -1,27 +1,26 @@
-import logging
-import os
 from collections.abc import Callable, Iterable
-from multiprocessing import JoinableQueue, Queue, Process
+from multiprocessing import JoinableQueue, Queue
 from typing import TypeVar
 
-from dagster import get_dagster_logger
 from rdflib import URIRef
-from tqdm import tqdm
 
 from graphs2go.models import interchange
 from graphs2go.namespaces.interchange import INTERCHANGE
+from graphs2go.transformers.parallel_transform import parallel_transform
 
-_INPUT_BATCH_SIZE = 100
+_INTERCHANGE_NODE_BATCH_SIZE = 100
 _OutputModelT = TypeVar("_OutputModelT")
 _TransformInterchangeNode = Callable[[interchange.Node], Iterable[_OutputModelT]]
+_InputT = tuple[interchange.Graph.Descriptor, URIRef, _TransformInterchangeNode]
 
 
 def _consumer(
-    interchange_graph_descriptor: interchange.Graph.Descriptor,
+    input_: _InputT,
     output_queue: Queue,
-    transform_interchange_node: _TransformInterchangeNode,
     work_queue: JoinableQueue,
 ) -> None:
+    (interchange_graph_descriptor, _, transform_interchange_node) = input_
+
     with interchange.Graph.open(
         interchange_graph_descriptor, read_only=True
     ) as interchange_graph:
@@ -39,15 +38,13 @@ def _consumer(
             output_queue.put(tuple(outputs))
             work_queue.task_done()
 
-    output_queue.put(None)  # Signal this consumer is done
-
 
 def _producer(
-    consumer_count: int,
-    interchange_graph_descriptor: interchange.Graph.Descriptor,
-    interchange_node_rdf_type: URIRef,
+    input_: _InputT,
     work_queue: JoinableQueue,
 ) -> None:
+    interchange_graph_descriptor, interchange_node_rdf_type, _ = input_
+
     interchange_node_uris_batch: list[URIRef] = []
     with interchange.Graph.open(
         interchange_graph_descriptor, read_only=True
@@ -56,17 +53,12 @@ def _producer(
             rdf_type=interchange_node_rdf_type
         ):
             interchange_node_uris_batch.append(interchange_node_uri)
-            if len(interchange_node_uris_batch) == _INPUT_BATCH_SIZE:
+            if len(interchange_node_uris_batch) == _INTERCHANGE_NODE_BATCH_SIZE:
                 work_queue.put(tuple(interchange_node_uris_batch))
                 interchange_node_uris_batch = []
 
     if interchange_node_uris_batch:
         work_queue.put(tuple(interchange_node_uris_batch))
-
-    for _ in range(consumer_count):
-        work_queue.put(None)  # Signal to the consumers there's no more work
-
-    work_queue.join()
 
 
 def transform_interchange_graph(
@@ -76,78 +68,22 @@ def transform_interchange_graph(
     interchange_node_rdf_type: URIRef = INTERCHANGE.Node,
     in_process: bool = False
 ) -> Iterable[_OutputModelT]:
-    logger = get_dagster_logger()
+    if in_process:
+        with interchange.Graph.open(
+            interchange_graph_descriptor, read_only=True
+        ) as interchange_graph:
+            for interchange_node in interchange_graph.nodes(
+                rdf_type=interchange_node_rdf_type
+            ):
+                yield from transform_interchange_node(interchange_node)
+        return
 
-    with tqdm(desc="output models", position=0) as output_model_tqdm:
-        if in_process:
-            with interchange.Graph.open(
-                interchange_graph_descriptor, read_only=True
-            ) as interchange_graph:
-                for interchange_node in tqdm(
-                    interchange_graph.nodes(rdf_type=interchange_node_rdf_type),
-                    desc="transformed interchange nodes",
-                    position=1,
-                ):
-                    for output_model in transform_interchange_node(interchange_node):
-                        output_model_tqdm.update(1)
-                        yield output_model
-            return
-
-        output_queue = Queue()
-        work_queue = JoinableQueue()
-
-        consumer_count = 1  # os.cpu_count()
-        logger.info(
-            "starting %d interchange graph transformation consumer processes",
-            consumer_count,
-        )
-        consumer_processes = tuple(
-            Process(
-                target=_consumer,
-                args=(
-                    interchange_graph_descriptor,
-                    output_queue,
-                    transform_interchange_node,
-                    work_queue,
-                ),
-            )
-            for _ in range(consumer_count)
-        )
-        for consumer_process in consumer_processes:
-            consumer_process.start()
-        logger.info(
-            "started %d interchange graph transformation consumer processes",
-            consumer_count,
-        )
-
-        logger.info("starting interchange graph transformation producer process")
-        producer_process = Process(
-            target=_producer,
-            args=(
-                consumer_count,
-                interchange_graph_descriptor,
-                interchange_node_rdf_type,
-                work_queue,
-            ),
-        )
-        producer_process.start()
-        logger.info("started interchange graph transformation producer process")
-
-        exited_consumer_count = 0
-        while True:
-            output_model_batch: tuple[_OutputModelT] | None = output_queue.get()
-            if output_model_batch is None:
-                exited_consumer_count += 1
-                logger.info(
-                    "%d interchange graph transformation consumers exited",
-                    exited_consumer_count,
-                )
-                if exited_consumer_count == consumer_count:
-                    break
-
-            output_model_tqdm.update(len(output_model_batch))
-            yield from output_model_batch
-
-        producer_process.join()
-        for consumer_process in consumer_processes:
-            consumer_process.join()
+    yield from parallel_transform(
+        consumer=_consumer,
+        input_=(
+            interchange_graph_descriptor,
+            interchange_node_rdf_type,
+            transform_interchange_node,
+        ),
+        producer=_producer,
+    )
