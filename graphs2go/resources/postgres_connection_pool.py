d2d274e94dd323462e67ff0bf149ad20efaf3f09
Minor Gordon <minorg@server.fake>
2024-03-27 17:08:57 -0400

    fix up umls

--- graphs2go/resources/postgres_connection_pool.py
+++ graphs2go/resources/postgres_connection_pool.py
@@ -34,7 +34,7 @@ class PostgresConnectionPool(ConfigurableResource):  # type: ignore
     def __enter__(self):
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type, exc_val, exc_tb):  # noqa: ANN001
         self.close()
 
     @classmethod
