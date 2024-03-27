62ea70e829195544e6bafb6c19604b93f2c71bf8
Minor Gordon <minorg@server.fake>
2024-03-27 17:48:27 -0400

    no dashes in namespace prefixes

--- graphs2go/namespaces/bind_namespaces.py
+++ graphs2go/namespaces/bind_namespaces.py
@@ -1,13 +1,19 @@
 from rdflib import Graph
 
+from graphs2go.namespaces.dash import DASH
 from graphs2go.namespaces.interchange import INTERCHANGE
 from graphs2go.namespaces.skosxl import SKOSXL
 from graphs2go.namespaces.snomed_schema import SNOMED_SCHEMA
 from graphs2go.namespaces.snomed_vocab import SNOMED_VOCAB
+from graphs2go.namespaces.vaem import VAEM
+from graphs2go.namespaces.voag import VOAG
 
 
 def bind_namespaces(graph: Graph) -> None:
+    graph.bind("dash", DASH)
     graph.bind("interchange", INTERCHANGE)
-    graph.bind("snomed-ct", SNOMED_VOCAB)
-    graph.bind("snomed-cts", SNOMED_SCHEMA)
+    graph.bind("snomedct", SNOMED_VOCAB)
+    graph.bind("snomedcts", SNOMED_SCHEMA)
     graph.bind("skosxl", SKOSXL)
+    graph.bind("vaem", VAEM)
+    graph.bind("voag", VOAG)
