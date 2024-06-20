b524adbee7f2ef44b56d76daf6c9546bcf37d01a
Minor Gordon <minorg@server.fake>
2024-06-19 21:13:58 -0400

    appease ruff

--- graphs2go/namespaces/__init__.py
+++ graphs2go/namespaces/__init__.py
@@ -1,4 +1,4 @@
 from .dash import DASH as DASH
-from .interchange import INTERCHANGE as INTER
+from .interchange import INTERCHANGE as INTERCHANGE
 from .namespaces import NAMESPACES as NAMESPACES
 from .skosxl import SKOSXL as SKOSXL
--- graphs2go/utils/__init__.py
+++ graphs2go/utils/__init__.py
@@ -3,5 +3,7 @@ from .find_directory_releases import find_directory_releases as find_directory_r
 from .find_file_releases import find_file_releases as find_file_releases
 from .hash_urn import hash_urn as hash_urn
 from .load_dotenv import load_dotenv as load_dotenv
-from .parse_directory_path_config_value import parse_directory_path_config_value
+from .parse_directory_path_config_value import (
+    parse_directory_path_config_value as parse_directory_path_config_value,
+)
 from .uuid_urn import uuid_urn as uuid_urn
