f2f071fbc78cd4487c62561c63433a30fc9087d3
Minor Gordon <minorg@server.fake>
2024-06-19 20:44:47 -0400

    add RDF format to output config

--- graphs2go/resources/output_config.py
+++ graphs2go/resources/output_config.py
@@ -5,23 +5,35 @@ from pathlib import Path
 
 from dagster import ConfigurableResource, EnvVar
 
+from graphs2go.models import rdf
+
 
 class OutputConfig(ConfigurableResource):  # type: ignore
     @dataclass(frozen=True)
     class Parsed:
         directory_path: Path
+        rdf_format: rdf.Format
 
     directory_path: str
+    rdf_format: str
 
     @classmethod
     def default(cls, *, directory_path_default: Path) -> OutputConfig:
-        return OutputConfig(directory_path=str(directory_path_default))
+        return OutputConfig(directory_path=str(directory_path_default), rdf_format="")
 
     @classmethod
     def from_env_vars(cls, *, directory_path_default: Path) -> OutputConfig:
         return cls(
             directory_path=EnvVar("GRAPHS2GO_OUTPUT_DIRECTORY_PATH").get_value(str(directory_path_default)),  # type: ignore
+            rdf_format=EnvVar("GRAPHS2GO_OUTPUT_RDF_FORMAT").get_value(""),  # type: ignore
         )
 
     def parse(self) -> Parsed:
-        return OutputConfig.Parsed(directory_path=Path(self.directory_path))
+        return OutputConfig.Parsed(
+            directory_path=Path(self.directory_path),
+            rdf_format=(
+                getattr(rdf.Format, self.rdf_format.upper())
+                if self.rdf_format
+                else rdf.Format.NTRIPLES
+            ),
+        )
